b50aad2b6fabb1684c9a5ad93134711c6bbadfe4
Blackenemy <enemyxiceblack@gmail.com>
2026-04-06 03:17:37 +0700

    fix: update PM2 start command to use ecosystem configuration for hubs-frontend

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,8 @@ module.exports = {
   apps: [
     {
       name: "hubs-frontend",
-      script: "node_modules/.bin/next",
-      args: "start",
-      cwd: "./apps/erp",
+      script: "node",
+      args: "node_modules/.bin/next start",
       instances: 1,
       exec_mode: "cluster",
       env: {
